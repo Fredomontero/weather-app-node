6d495aad1c458b8a0cbd9d189cda4d891d17aae8
Fredomontero <christianmontero08@gmail.com>
2020-10-30 22:09:15 -0700

    Feat: refactor request weather method

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const request = require('request');
 
-const url = 'http://api.weatherstack.com/current?access_key=ab15cc2abc82aa9834b1e63964b339fe&query=37.8267,-122.4233';
+const url = 'http://api.weatherstack.com/current?access_key=ab15cc2abc82aa9834b1e63964b339fe&query=32.4968,-117.0178';
 
-request({ url }, (error, response) => {
-  const data = JSON.parse(response.body);
-  console.log(data.current);
+request({ url, json: true }, (error, response) => {
+  let weather = response.body.current;
+  console.log(`It's currently ${weather.temperature} degrees out. There is a ${weather.precip}% chance of rain`);
 });
