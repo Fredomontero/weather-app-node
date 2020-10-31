0a97bc9a79246a53b355ab7758de75021e51ac87
Fredomontero <christianmontero08@gmail.com>
2020-10-31 13:16:31 -0700

    Feat: Add error andling

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
 const request = require('request');
 
+const weatherUrl = 'http://api.weatherstack.com/current?access_key=ab15cc2abc82aa9834b1e63964b339fe&query=32.4968,-117.0178';
+
+// request({ url: weatherUrl, json: true }, (error, response) => {
+//   if(error){
+//     console.log("Unable to connect to the weather service");
+//   }else if(response.body.error){
+//     console.log("Unable to find location");
+//   }else{
+//     let weather = response.body.current;
+//     console.log(`It's currently ${weather.temperature} degrees out. There is a ${weather.precip}% chance of rain`);
+//   }
+// });
+
 const geoCodeUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiY2htYXJ0aW5leiIsImEiOiJja2d4OXdleWYwaHlmMnFwaWh3ZGNsNWRrIn0.HZ2GylbDeNC9w1PADQAk3w&limit=1';
 
 request({ url: geoCodeUrl, json: true }, (error, response) => {
-  let latitude = response.body.features[0].center[1];
-  let longitude = response.body.features[0].center[0];
-  // console.log(`The coordinate of LA are:  ${coordinates}`);
-  console.log(latitude, longitude);
+  if(error){
+    console.log("Unable to connect to the location service");
+  }else if(response.body.features.length === 0){
+    console.log("Unable to find location")
+  }else{
+    let latitude = response.body.features[0].center[1];
+    let longitude = response.body.features[0].center[0];
+  }
+  
 });
 
-const weatherUrl = 'http://api.weatherstack.com/current?access_key=ab15cc2abc82aa9834b1e63964b339fe&query=32.4968,-117.0178';
-
-request({ url: weatherUrl, json: true }, (error, response) => {
-  let weather = response.body.current;
-  console.log(`It's currently ${weather.temperature} degrees out. There is a ${weather.precip}% chance of rain`);
-});
