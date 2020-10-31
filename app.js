99aa800411b277c27a2d20ab0e6d5e8fb2d14a4a
Fredomontero <christianmontero08@gmail.com>
2020-10-30 23:32:45 -0700

    Feat: Add request for forward geocoding

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const request = require('request');
 
-const url = 'http://api.weatherstack.com/current?access_key=ab15cc2abc82aa9834b1e63964b339fe&query=32.4968,-117.0178';
+const geoCodeUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiY2htYXJ0aW5leiIsImEiOiJja2d4OXdleWYwaHlmMnFwaWh3ZGNsNWRrIn0.HZ2GylbDeNC9w1PADQAk3w&limit=1';
 
-request({ url, json: true }, (error, response) => {
+request({ url: geoCodeUrl, json: true }, (error, response) => {
+  let latitude = response.body.features[0].center[1];
+  let longitude = response.body.features[0].center[0];
+  // console.log(`The coordinate of LA are:  ${coordinates}`);
+  console.log(latitude, longitude);
+});
+
+const weatherUrl = 'http://api.weatherstack.com/current?access_key=ab15cc2abc82aa9834b1e63964b339fe&query=32.4968,-117.0178';
+
+request({ url: weatherUrl, json: true }, (error, response) => {
   let weather = response.body.current;
   console.log(`It's currently ${weather.temperature} degrees out. There is a ${weather.precip}% chance of rain`);
 });
