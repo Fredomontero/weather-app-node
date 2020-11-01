d1545a3218b8a01df47dc8a066ed9fa59e4c55b4
Fredomontero <christianmontero08@gmail.com>
2020-11-01 00:27:05 -0700

    feat: add functionality to get the location from command line

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
+const location = process.argv[2];
+if(location){
+  geocode(location, (error, data) => {
+    if(error){
+      return console.log("Error: ", error);
+    }
+    forecast(data.latitude , data.longitude, (error, forecastData) => {
+      if(error){
+        return console.log("Error: ", error);
+      }
+      console.log(`The temperature in is ${forecastData.temperature} and is ${forecastData.precipitation}% chance to rain`);
+    });
+  });
+}else{
+  console.log("Please provide a location");
+}
 
-geocode('Tijuana', (error, data) => {
-  console.log("Error: ", error);
-  console.log("Data: ", data);
-});
-
-forecast(32.4968, -117.0178, (error, data) => {
-  console.log("Error: ", error);
-  console.log("Data: ", data);
-});
 
--- utils/forecast.js
+++ utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request');
 
 const forecast = (latitude, longitude, callback) => {
-  const url = 'http://api.weatherstack.com/current?access_key=ab15cc2abc82aa9834b1e63964b339fe&query=' + latitude + ',' + longitude
+  const url = 'http://api.weatherstack.com/current?access_key=ab15cc2abc82aa9834b1e63964b339fe&query=' + latitude + ',' + longitude;
   request({ url, json: true }, (error, response) => {
     if(error){
       callback("Unable to connect to the weather service", undefined)
