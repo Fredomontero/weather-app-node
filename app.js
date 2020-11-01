28319e5c6b45a7616ae55e76a3cb4011256edb25
Fredomontero <christianmontero08@gmail.com>
2020-10-31 18:02:13 -0700

    Feat: Refactor geocode functionality

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-const request = require('request');
+const geocode = require('./utils/geocode');
 
-const weatherUrl = 'http://api.weatherstack.com/current?access_key=ab15cc2abc82aa9834b1e63964b339fe&query=32.4968,-117.0178';
+
+// const weatherUrl = 'http://api.weatherstack.com/current?access_key=ab15cc2abc82aa9834b1e63964b339fe&query=32.4968,-117.0178';
 
 // request({ url: weatherUrl, json: true }, (error, response) => {
 //   if(error){
@@ -13,17 +14,25 @@ const weatherUrl = 'http://api.weatherstack.com/current?access_key=ab15cc2abc82a
 //   }
 // });
 
-const geoCodeUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiY2htYXJ0aW5leiIsImEiOiJja2d4OXdleWYwaHlmMnFwaWh3ZGNsNWRrIn0.HZ2GylbDeNC9w1PADQAk3w&limit=1';
+// const geoCodeUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiY2htYXJ0aW5leiIsImEiOiJja2d4OXdleWYwaHlmMnFwaWh3ZGNsNWRrIn0.HZ2GylbDeNC9w1PADQAk3w&limit=1';
 
-request({ url: geoCodeUrl, json: true }, (error, response) => {
-  if(error){
-    console.log("Unable to connect to the location service");
-  }else if(response.body.features.length === 0){
-    console.log("Unable to find location")
-  }else{
-    let latitude = response.body.features[0].center[1];
-    let longitude = response.body.features[0].center[0];
-  }
+// request({ url: geoCodeUrl, json: true }, (error, response) => {
+//   if(error){
+//     console.log("Unable to connect to the location service");
+//   }else if(response.body.features.length === 0){
+//     console.log("Unable to find location")
+//   }else{
+//     let latitude = response.body.features[0].center[1];
+//     let longitude = response.body.features[0].center[0];
+//     console.log("Latitude: ", latitude)
+//   }
   
+// });
+
+
+
+geocode('Tijuana', (error, data) => {
+  console.log("Error: ", error);
+  console.log("Data: ", data);
 });
 
