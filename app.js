f284df4cbfdfe954b9a6b50f2fc6b3f878d058b7
Fredomontero <christianmontero08@gmail.com>
2020-11-01 12:05:51 -0800

    Feat: Refactor forecast and geocode with destructuring and default parameters

--- app.js
+++ app.js
@@ -3,15 +3,15 @@ const forecast = require('./utils/forecast');
 
 const location = process.argv[2];
 if(location){
-  geocode(location, (error, data) => {
+  geocode(location, (error, { latitude, longitude } = {}) => {
     if(error){
       return console.log("Error: ", error);
     }
-    forecast(data.latitude , data.longitude, (error, forecastData) => {
+    forecast(latitude , longitude, (error, { temperature, precipitation }) => {
       if(error){
         return console.log("Error: ", error);
       }
-      console.log(`The temperature in is ${forecastData.temperature} and is ${forecastData.precipitation}% chance to rain`);
+      console.log(`The temperature in is ${temperature} and is ${precipitation}% chance to rain`);
     });
   });
 }else{
--- utils/forecast.js
+++ utils/forecast.js
@@ -2,15 +2,15 @@ const request = require('request');
 
 const forecast = (latitude, longitude, callback) => {
   const url = 'http://api.weatherstack.com/current?access_key=ab15cc2abc82aa9834b1e63964b339fe&query=' + latitude + ',' + longitude;
-  request({ url, json: true }, (error, response) => {
+  request({ url, json: true }, (error, { body }) => {
     if(error){
       callback("Unable to connect to the weather service", undefined)
-    }else if(response.body.error){
+    }else if(body.error){
       callback("Unable to find location. Try another search", undefined)
     }else{
       callback(undefined, {
-        temperature: response.body.current.temperature,
-        precipitation: response.body.current.precip
+        temperature: body.current.temperature,
+        precipitation: body.current.precip
       });
     }
   });
